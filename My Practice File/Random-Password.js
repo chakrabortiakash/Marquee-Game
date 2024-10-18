cc289658eaaf369fc18c98166afed464f8187db4
Akash kumar <akashchakraborti1998@gmail.com>
2024-10-18 23:27:12 +0530

    Random-Password

---
+++ My Practice File/Random-Password.js
@@ -0,0 +1,23 @@
+
+// const passwordBox = document.getElementById("password");
+// const lenght = 12;
+
+// const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+// const number    = "0123456789"
+// const symbol    = "@$%#&*()_+~|{}[]></-=";
+
+// const allChars = upperCase + lowerCase + number + symbol;
+
+// function createPassword (){
+//     let password = "";
+//     password += upperCase[math.flood(Math.random() * upperCase.length)];
+//     password += lowerCase[math.flood(Math.random() * loweerCase.length)];
+//     password += number [math.flood(Math.random() * number.length)];
+//     password += symbol [math.flood(Math.random() * symbol.length)];
+
+//     while(lenght > password.length){
+//         password += allChars [math.floor(Math.random() * allChars.length)];
+//     }
+//     passwordBox.value = password;
+// }
